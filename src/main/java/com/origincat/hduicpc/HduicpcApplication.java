f2758a30b752929595e834ccadf7d8fd8ceae2c0
origincat <truealfajin@gmail.com>
2020-07-03 01:47:55 +0800

    create

---
+++ src/main/java/com/origincat/hduicpc/HduicpcApplication.java
@@ -0,0 +1,13 @@
+package com.origincat.hduicpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HduicpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HduicpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/origincat/hduicpc/HduicpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.origincat.hduicpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HduicpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
